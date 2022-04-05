d2254d7c56164afbda2c92674bb7a282b297d15a
giacomocerquone <cerquone96@hotmail.it>
2022-04-05 10:56:16 +0200

    first changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          placeholder="Inserisci il tuo nome"
+          onChange={(e) => {
+            setName(e.target.value.toUpperCase());
+          }}
+          value={name}
+        />
+
+        <p>Il tuo nome modificato:</p>
+
+        <p>{name}</p>
       </header>
     </div>
   );
