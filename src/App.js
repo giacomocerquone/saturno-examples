b6f9010e4540ef64b59a057700f30778f7a2342f
giacomocerquone <cerquone96@hotmail.it>
2022-04-08 16:12:02 +0200

    learning useeffect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,34 @@ import client from "./services/client";
 
 function App() {
   const [image, setImage] = useState("");
+  const [date, setDate] = useState(Date.now());
+
+  // This useEffect runs only at mount time (e.g. when the component is first rendered)
+  // registers an interval where each 3 seconds we update the dateState
+  useEffect(() => {
+    console.log("running the effect");
+
+    setInterval(() => {
+      setDate(Date.now());
+    }, 3000);
+  }, []);
+
+  // This fetches a cat each 3 seconds
+  // but to showcase the usage of the second useeffect parameter
+  // we're gonna take a look at the above one
+
+  // useEffect(() => {
+  //   const fetchCat = () => {
+  //     setInterval(async () => {
+  //       const stream = await client("images/search");
+  //       const data = await stream.json();
+
+  //       setImage(data?.[0]?.url);
+  //     }, 3000);
+  //   };
+
+  //   fetchCat();
+  // }, []);
 
   const requestCat = async () => {
     const stream = await client("images/search");
@@ -16,6 +44,19 @@ function App() {
     setImage(data?.[0]?.url);
   };
 
+  // Here we register a useEffect where each time the date state change
+  // we issue a new cat request
+  useEffect(() => {
+    const fetchCat = async () => {
+      const stream = await client("images/search");
+      const data = await stream.json();
+
+      setImage(data?.[0]?.url);
+    };
+
+    fetchCat();
+  }, [date]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -26,5 +67,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
